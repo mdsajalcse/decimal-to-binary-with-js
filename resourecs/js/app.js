e56eb585c26d5729053b105ad9f34af3d9cb051a
ithekar sajal <sajalcse2018@gmail.com>
2023-02-23 16:40:35 +0600

    js file added

---
+++ resourecs/js/app.js
@@ -0,0 +1,16 @@
+// const decimalNumber=(document.getElementById('input1'));
+//      const resultBinary=(doucment.getElementById('result'));
+
+document.getElementById('myBtn').addEventListener('click',function() {
+     
+    console.log("clicked");
+     
+
+    // const FirstValue=decimalNumber.value;
+
+    // if(decimalNumber.value==''){
+    // alert('please enter valid value');
+
+
+
+})
